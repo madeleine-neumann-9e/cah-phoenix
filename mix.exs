e6d7c2757f19145510230c366b4a82e9b8c436a9
Sascha Brink <sascha.brink@gmail.com>
2020-04-02 21:10:11 +0200

    Add s alias

--- mix.exs
+++ mix.exs
@@ -56,7 +56,8 @@ defmodule Platform.MixProject do
     [
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate", "test"]
+      test: ["ecto.create --quiet", "ecto.migrate", "test"],
+      s: ["phx.server"]
     ]
   end
 end
