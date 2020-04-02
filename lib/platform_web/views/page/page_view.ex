0bfed64a5a879d0699179b0693a577b6241c6a15
Madeleine Neumann <madeleine.neumann@9elements.com>
2020-04-03 00:56:41 +0200

    adds style to regex

--- lib/platform_web/views/page/page_view.ex
+++ lib/platform_web/views/page/page_view.ex
@@ -2,6 +2,6 @@ defmodule PlatformWeb.PageView do
   use PlatformWeb, :view
 
   def replace_placeholder(text) do
-    text |> String.replace("_", "<h1>.....</h1>")
+    text |> String.replace("_", ~s(<span style="color: red">[...]</span>))
   end
 end
