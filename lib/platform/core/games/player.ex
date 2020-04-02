c94a951b9e035c0209f825be93a2110bf038d990
Sascha Brink <sascha.brink@gmail.com>
2020-04-02 23:32:42 +0200

    Add random white cards

--- lib/platform/core/games/player.ex
+++ lib/platform/core/games/player.ex
@@ -1,11 +1,16 @@
 defmodule Platform.Games.Player do
   defstruct [:id, :name, :all_white_cards, :points, :reader]
+  alias Platform.Cards
 
   def new(name) do
+    white_cards =
+      Cards.list_white_cards()
+      |> Enum.take_random(10)
+
     %__MODULE__{
       id: :rand.uniform(10000),
       name: name,
-      all_white_cards: [],
+      all_white_cards: white_cards,
       points: 0
     }
   end
