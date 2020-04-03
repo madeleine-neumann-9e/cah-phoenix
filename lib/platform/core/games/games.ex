0c53abf39c58d63c759f6a05dfc88efa7d473ff5
Sascha Brink <sascha.brink@gmail.com>
2020-04-03 22:39:40 +0200

    Add reset check

--- lib/platform/core/games/games.ex
+++ lib/platform/core/games/games.ex
@@ -51,4 +51,8 @@ defmodule Platform.Games do
       nil
     end
   end
+
+  def show_reset_button?(%Game{} = _game, %Player{selected_white_cards: selected_white_cards}) do
+    length(selected_white_cards) > 0
+  end
 end
