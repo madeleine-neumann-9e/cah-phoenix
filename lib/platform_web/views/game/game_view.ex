aa3877657e227e0ac57a698a0890ad01880c49f3
Sascha Brink <sascha.brink@gmail.com>
2020-04-11 22:20:57 +0200

    Improve replace placeholder

--- lib/platform_web/views/game/game_view.ex
+++ lib/platform_web/views/game/game_view.ex
@@ -4,7 +4,15 @@ defmodule PlatformWeb.GameView do
   alias Platform.Games
   alias Platform.Players
 
-  def replace_placeholder(text) do
-    text |> String.replace("_", ~s(<span style="color: red">[...]</span>))
+  def replace_placeholder(black_card, []) do
+    black_card |> String.replace("_", ~s(<span style="color: red">[...]</span>))
+  end
+  def replace_placeholder(text, white_cards) do
+    if String.contains?(text, "_") do
+      text |> String.replace("_", ~s(<span style="color: red">[...]</span>))
+    else
+      white_card = white_cards |> List.first
+      text <> ~s(<span style="color: red">#{white_card.answer}</span>)
+    end
   end
 end
